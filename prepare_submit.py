b59d9d15fdf3f474ec36c780d7d4a500b1762f43
Jiatai Li <jiatail@andrew.cmu.edu>
2024-02-15 01:50:47 -0500

    chore: better submit process

--- prepare_submit.py
+++ prepare_submit.py
@@ -22,6 +22,33 @@ required_files = {'run_llama.py',
                   'cfimdb-dev-finetuning-output.txt',
                   'cfimdb-test-finetuning-output.txt'}
 
+packed_files = {
+    "run_llama.py",
+    "base_llama.py",
+    "llama.py",
+    "rope.py",
+    "classifier.py",
+    "config.py",
+    "optimizer.py",
+    "sanity_check.py",
+    "tokenizer.py",
+    "utils.py",
+    "README.md",
+    "structure.md",
+    "sanity_check.data",
+    "generated-sentence-temp-0.txt",
+    "generated-sentence-temp-1.txt",
+    "sst-dev-prompting-output.txt",
+    "sst-test-prompting-output.txt",
+    "sst-dev-finetuning-output.txt",
+    "sst-test-finetuning-output.txt",
+    "cfimdb-dev-prompting-output.txt",
+    "cfimdb-test-prompting-output.txt",
+    "cfimdb-dev-finetuning-output.txt",
+    "cfimdb-test-finetuning-output.txt",
+    "setup.sh",
+}
+
 def check_file(file: str, check_aid: str):
     target_prefix = None
     # --
@@ -61,13 +88,12 @@ def main(path: str, aid: str):
                 if '.git' in root or '__pycache__' in root:
                     continue  # ignore some parts
                 for file in files:
-                    if file.endswith(".zip"):
-                        continue
                     ff = os.path.join(root, file)
                     rpath = os.path.relpath(ff, path)
-                    zz.write(ff, os.path.join(".", aid, rpath))
-                    if rpath in required_files:
-                        required_files.remove(rpath)
+                    if rpath in packed_files:
+                        zz.write(ff, os.path.join(".", aid, rpath))
+                        if rpath in required_files:
+                            required_files.remove(rpath)
         assert len(required_files) == 0, breakpoint()
         # --
         print(f"Submission zip file created from DIR={path} for {aid}: {aid}.zip")
